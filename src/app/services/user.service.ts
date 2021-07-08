03b7d0c39662aed7157b7b0b80cb99192f16059b
rineshkm <rinesh.km@accionlabs.com>
2021-07-08 21:46:20 +0530

    Update env var

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,17 +5,13 @@ import { User } from '../models/user';
 
 @Injectable()
 export class UserService {
+
+  //api URL 
     readonly rootUrl = 'http://localhost:59318/api/';
     constructor(private http: HttpClient) { }
   
     registerUser(user: User) {
-      const body: User = {
-        UserName: user.Email,
-        Password: user.Password,
-        Email: user.Email,
-        FirstName: user.FirstName,
-        LastName: user.LastName
-      }
+      
       var data = {
         UserName: user.Email,
         Password: user.Password,
@@ -24,13 +20,9 @@ export class UserService {
       var reqHeader = new HttpHeaders({'No-Auth':'True'});
       return this.http.post(this.rootUrl + 'Users', data);
     }
-
     
     userAuthentication(userName: string, password: string) {
-      //var data = "username=" + userName + "&password=" + password;
       var data ={userName : userName, password : password };
-      //var reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded','No-Auth':'True' });
-      //var result =  this.http.post(this.rootUrl + 'Users/Authenticate', data, { headers: reqHeader });
       var result =  this.http.post(this.rootUrl + 'Users/Authenticate', data);
       return this.http.post(this.rootUrl + 'Users/Authenticate', data);
     }
